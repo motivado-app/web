feba68f48ee01299a289566213acfcdc6fa9a760
bartick <bartickmaiti1998@gmail.com>
2022-01-03 13:19:27 +0530

    Transfer push

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,15 +4,13 @@ import logo from './../images/motivado_logo.png'
 
 export default function Landing() {
     return (
-        <div>
-            <div className="container flex flex-col md:flex-row content-center space-x-14">
-                <img src={newProject} alt="Motivado" className="xl:w-[53%] xl:h-[53%] md:w-[60%] md:h-[60%] w-[90%] h-[90%]" />
-                <p className='hidden lg:contents'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-                <div className='flex flex-col justify-center items-center space-y-4'>
-                    <img src={logo} alt="Motivato" className='w-[14rem] h-[14rem]' />
-                    <h1 className='text-7xl font-bold hidden md:contents'>Motivado</h1>
-                    <p className='text-lg text-gray-500 font-light text-center'>" Today is your opportunity to build the tomorrow you want "</p>
-                </div>
+        <div className="container flex flex-col md:flex-row md:content-center md:justify-center space-x-14">
+            <img src={newProject} alt="Motivado" className="xl:w-[53%] xl:h-[53%] md:w-[60%] md:h-[60%] w-[90%] h-[90%] hidden md:contents" />
+            <p className='hidden md:contents'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+            <div className='flex flex-col justify-center items-center space-y-4'>
+                <img src={logo} alt="Motivato" className='w-[14rem] h-[14rem]' />
+                <h1 className='text-6xl md:text-7xl font-bold'>Motivado</h1>
+                <p className='text-lg text-gray-500 font-light text-center'>" Today is your opportunity to build the tomorrow you want "</p>
             </div>
         </div>
     )
---
+++ src/components/Teams.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Teams() {
+    const members = [
+        {
+            name: 'Advaita Saha',
+            description: 'Developer',
+            image: 'https://avatars.githubusercontent.com/u/30210770',
+            link: 'https://www.linkedin.com/in/advaita-saha/',
+        },
+        {
+            name: 'Bartick Maiti',
+            description: 'Developer',
+            image: 'https://avatars.githubusercontent.com/u/69100224',
+            link: 'https://www.linkedin.com/in/bartick-maiti-83952721a/',
+        },
+        {
+            name: 'Arka Roy',
+            description: 'Public Relations',
+            image: require('./../images/arko_roy_pr.jpeg'),
+            link: 'https://www.linkedin.com/in/arka-roy-057081223',
+        },
+        {
+            name: 'Anushka Kohli',
+            description: 'Public Relations',
+            image: require('./../images/anuska.jpeg'),
+            link: 'https://www.linkedin.com/in/anushka-kohli-9410771b8',
+        },
+    ]
+    return (
+        <div>
+            
+        </div>
+    )
+}
